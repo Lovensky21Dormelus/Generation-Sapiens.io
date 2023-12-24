9e7fed78f73e3c6343bf5612e6435621b23287aa
Lovensky21Dormelus <154761530+Lovensky21Dormelus@users.noreply.github.com>
2023-12-24 12:41:28 -0800

    Add files via upload

---
+++ Quiz/Quizgame.js
@@ -0,0 +1,196 @@
+const quizData = [
+    {
+      question: 'Quelle est la capitale de la France',
+      options: ['Paris', 'Londres', 'Berlin', 'Madrid'],
+      answer: 'Paris',
+    },
+    {
+      question: 'Quelles est la plus grosse planete du systeme solaire?',
+      options: ['Mars', 'Saturne', 'Jupiter', 'Neptune'],
+      answer: 'Jupiter',
+    },
+    {
+      question: 'Quel est le plus grand pays du monde?',
+      options: ['Brazil', 'Belgique', 'France', 'Russie'],
+      answer: 'France',
+    },
+    {
+      question: 'Quelle est la plus haute montagne du monde?',
+      options: ['Mount Everest', 'K2', 'Kangchenjunga', 'Makalu'],
+      answer: 'Mount Everest',
+    },
+    {
+      question: 'Le plus vaste ocean du monde?',
+      options: [
+        'Ocean Pacifique',
+        'Ocean Indien',
+        ' Ocean Atlantique ',
+        ' Ocean Artique',
+      ],
+      answer: ' Ocean Pacifique',
+    },
+    {
+      question: 'What is the chemical symbol for gold?',
+      options: ['Au', 'Ag', 'Cu', 'Fe'],
+      answer: 'Au',
+    },
+    {
+      question: 'Who painted the Mona Lisa?',
+      options: [
+        'Pablo Picasso',
+        'Vincent van Gogh',
+        'Leonardo da Vinci',
+        'Michelangelo',
+      ],
+      answer: 'Leonardo da Vinci',
+    },
+    {
+      question: 'Which planet is known as the Red Planet?',
+      options: ['Mars', 'Venus', 'Mercury', 'Uranus'],
+      answer: 'Mars',
+    },
+    {
+      question: 'What is the largest species of shark?',
+      options: [
+        'Great White Shark',
+        'Whale Shark',
+        'Tiger Shark',
+        'Hammerhead Shark',
+      ],
+      answer: 'Whale Shark',
+    },
+    {
+      question: 'Le plus grand desert du monde?',
+      options: ['Le desert du Magreb', 'Le Sahara', 'La pampa', 'Le desert de Gobi'],
+      answer: 'Lion',
+    },
+  ];
+  
+  const quizContainer = document.getElementById('quiz');
+  const resultContainer = document.getElementById('result');
+  const submitButton = document.getElementById('submit');
+  const retryButton = document.getElementById('retry');
+  const showAnswerButton = document.getElementById('showAnswer');
+  
+  let currentQuestion = 0;
+  let score = 0;
+  let incorrectAnswers = [];
+  
+  function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+  }
+  
+  function displayQuestion() {
+    const questionData = quizData[currentQuestion];
+  
+    const questionElement = document.createElement('div');
+    questionElement.className = 'question';
+    questionElement.innerHTML = questionData.question;
+  
+    const optionsElement = document.createElement('div');
+    optionsElement.className = 'options';
+  
+    const shuffledOptions = [...questionData.options];
+    shuffleArray(shuffledOptions);
+  
+    for (let i = 0; i < shuffledOptions.length; i++) {
+      const option = document.createElement('label');
+      option.className = 'option';
+  
+      const radio = document.createElement('input');
+      radio.type = 'radio';
+      radio.name = 'quiz';
+      radio.value = shuffledOptions[i];
+      radio.className="radio-button";
+  
+      const optionText = document.createTextNode(shuffledOptions[i]);
+  
+      option.appendChild(radio);
+      option.appendChild(optionText);
+      optionsElement.appendChild(option);
+    }
+  
+    quizContainer.innerHTML = '';
+    quizContainer.appendChild(questionElement);
+    quizContainer.appendChild(optionsElement);
+  }
+  
+  function checkAnswer() {
+    const selectedOption = document.querySelector('input[name="quiz"]:checked');
+    if (selectedOption) {
+      const answer = selectedOption.value;
+      if (answer === quizData[currentQuestion].answer) {
+        score++;
+      } else {
+        incorrectAnswers.push({
+          question: quizData[currentQuestion].question,
+          incorrectAnswer: answer,
+          correctAnswer: quizData[currentQuestion].answer,
+        });
+      }
+      currentQuestion++;
+      selectedOption.checked = false;
+      if (currentQuestion < quizData.length) {
+        displayQuestion();
+      } else {
+        displayResult();
+      }
+    }
+  }
+  
+  function displayResult() {
+    quizContainer.style.display = 'none';
+    submitButton.style.display = 'none';
+    retryButton.style.display = 'inline-block';
+    showAnswerButton.style.display = 'inline-block';
+    resultContainer.innerHTML = `You scored ${score} out of ${quizData.length}!`;
+  }
+  
+  function retryQuiz() {
+    currentQuestion = 0;
+    score = 0;
+    incorrectAnswers = [];
+    quizContainer.style.display = 'block';
+    submitButton.style.display = 'inline-block';
+    retryButton.style.display = 'none';
+    showAnswerButton.style.display = 'none';
+    resultContainer.innerHTML = '';
+    displayQuestion();
+  }
+  
+  function showAnswer() {
+    quizContainer.style.display = 'none';
+    submitButton.style.display = 'none';
+    retryButton.style.display = 'inline-block';
+    showAnswerButton.style.display = 'none';
+  
+    let incorrectAnswersHtml = '';
+    for (let i = 0; i < incorrectAnswers.length; i++) {
+      incorrectAnswersHtml += `
+        <p>
+          <strong>Question:</strong> ${incorrectAnswers[i].question}<br>
+          <strong>Your Answer:</strong> ${incorrectAnswers[i].incorrectAnswer}<br>
+          <strong>Correct Answer:</strong> ${incorrectAnswers[i].correctAnswer}
+        </p>
+      `;
+    }
+  
+    resultContainer.innerHTML = `
+      <p>You scored ${score} out of ${quizData.length}!</p>
+      <p>Incorrect Answers:</p>
+      ${incorrectAnswersHtml}
+    `;
+    resultContainer.id="result-Container"
+  }
+  
+  submitButton.addEventListener('click', checkAnswer);
+  retryButton.addEventListener('click', retryQuiz);
+  showAnswerButton.addEventListener('click', showAnswer);
+  
+  displayQuestion();
+  Final
+  
+  
